b2543c7256ef8ed15c7cc66729d7fc22d6eda642
Gabriel Fidelis de Sousa <gabriel.fidelis.sousa@gmail.com>
2019-12-20 05:52:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["Gabriel" ,"Bibi"]
-  bibi_element = remove_element_from_end_of_array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
